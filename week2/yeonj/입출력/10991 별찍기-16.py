f5024a8980d18e0f8dc2823d444a73111c583174
yeon-jae <lisanam99@naver.com>
2024-03-26 15:46:02 +0900

    yj: 입출력 10991 10992

--- week2/yeonj/입출력/10991 별찍기-16.py
+++ week2/yeonj/입출력/10991 별찍기-16.py
@@ -1,6 +1,3 @@
-n = int(input())
-for i in range(1,n+1):
-    for j in range(2,n):
-        print((2*i-1)*' ')
-    print((n-i)*' '+'*'*(2*i-1))    
-
+n = int(input("Enter a number: "))
+for i in range(1, n + 1):
+    print((n - i) * ' ' + '* ' * i)
