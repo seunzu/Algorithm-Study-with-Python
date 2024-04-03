c16a958c2bd25ea7b183f865a6c62ef89f6f075f
zzoo0123 <0123suh@gmail.com>
2024-04-03 23:35:32 +0900

    sj: 정렬 2751 11650 11651

---
+++ week 3/seunzu/정렬/11650 좌표 정렬하기.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+arr = []
+
+for _ in range(n):
+    [x, y] = map(int, input().split())
+    arr.append([x, y])
+
+arr.sort(key=lambda x: (x[0], x[1]))
+
+for i in arr:
+    print(i[0], i[1])
+
