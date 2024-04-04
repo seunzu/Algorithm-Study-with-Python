e67b8c71583c811fe1c77566f73b94a1d2184b8f
zzoo0123 <0123suh@gmail.com>
2024-04-04 17:23:55 +0900

    sj: 정렬 10814 10825 10989 11004 11652

---
+++ week 3/seunzu/정렬/11652 카드.py
@@ -0,0 +1,14 @@
+n = int(input())
+dic = {}
+
+for _ in range(n):
+    num = int(input())
+
+    if dic.get(num, 0):
+        dic[num] += 1
+    else:
+        dic[num] = 1
+
+dic = sorted(dic.items(), key=lambda x: (-x[1], x[0]))
+
+print(dic[0][0])
